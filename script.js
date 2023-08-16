652b43faab47472eab79e8b0e6ab0ce3eca4d6b1
ninipp <parfaitnininahazwe@gmail.com>
2023-08-16 15:37:09 +0200

    Drumkit finished

---
+++ script.js
@@ -0,0 +1,28 @@
+
+function playSound(event) {
+    const key = document.querySelector(`div[data-key='${event.keyCode}']`);
+    const audio = document.querySelector(`audio[data-key='${event.keyCode}']`);
+    if (!audio) return;
+    const root = document.querySelector(":root");
+    const red = Math.round(Math.random() * 255);
+    const blue = Math.round(Math.random() * 255);
+    const green = Math.round(Math.random() * 255);
+
+    root.style.setProperty("--color", `rgb(${red}, ${green}, ${blue})`);
+
+    audio.currentTime = 0;
+    key.classList.add("playing");
+    audio.play();
+
+}
+
+function removeClass(event) {
+    event.target.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => {
+    key.addEventListener("transitionend", removeClass);
+});
+
+window.addEventListener("keydown", playSound);
